59ff9f5139c0bb154698af86aae71281c6be1446
zhangziqiao <zhangziqiao@kuainiugroup.com>
2020-12-08 13:27:03 +0800

    最长公共前缀

---
+++ array/longest-common-prefix.go
@@ -0,0 +1,31 @@
+package array
+
+import (
+	"fmt"
+	"strings"
+)
+
+/** 14
+* 编写一个函数来查找字符串数组中的最长公共前缀。如果不存在公共前缀，则返回""。
+* 输入: ["flower","flow","flight"]
+* 输出: "fl"
+*
+* 分析
+* 拆分元素依次对比
+**/
+func LongestCommonPrefix(str []string) string {
+	if len(str) <= 0 {
+		return ""
+	}
+	first := str[0]
+	for _,v := range str {
+		for strings.Index(v,first) != 0 {
+			fmt.Println(v)
+			if len(first)  == 0{
+				return ""
+			}
+			first = first[:len(first) -1]
+		}
+	}
+	return first
+}
--- main.go
+++ main.go
@@ -3,14 +3,10 @@ package main
 import (
 	"algorithm/array"
 	"fmt"
-	"sort"
 )
 
 func main()  {
-	sliceNum1 :=[]int{1,2,2,1}
+	strSlice :=[]string{"abcwqwda1","abcdafwad2","abbdf233"}
 
-	sliceNum2 :=[]int{2,2}
-	sort.Ints(sliceNum1)
-	sort.Ints(sliceNum2)
-	fmt.Println(array.IntersectPlus(sliceNum1,sliceNum2))
+	fmt.Println(array.LongestCommonPrefix(strSlice))
 }
